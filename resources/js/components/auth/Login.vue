3c08ca52d6cc66edf93f8125a633be53006b2829
Franco <jnarvaez@est.frba.utn.edu.ar>
2020-01-26 17:07:57 -0300

    Creación del componente Login
    Este componente maneja el layout de inicio de
    sesion y el llamado a la API asincrónica (por Vuex)
    con los datos que ingresa el usuario.

---
+++ resources/js/components/auth/Login.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="login row justify-content-center">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Login
+                </div>
+                <div class="card-body">
+                    <form @submit.prevent="authenticate">
+                        <div class="form-group row">
+                            <label for="email">Email:</label>
+                            <input type="email" v-model="form.email" class="form-control" placeholder="Ingrese su email">
+                        </div>
+                        <div class="form-group row">
+                            <label for="password">Password:</label>
+                            <input type="password" v-model="form.password" class="form-control" placeholder="Ingrese su contraseña">
+                        </div>
+                        <div class="form-group row">
+                            <input type="submit" value="Login">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+import {login} from '../../helpers/auth';
+
+export default {
+    name:'Login',
+    data(){
+        return{
+            form:{
+                email:'',
+                password:''
+            },
+            error: null
+        }
+    },
+    methods:{
+        authenticate(){
+            this.$store.dispatch('login');
+
+            login(this.$data.form).then(
+                (res) => {
+                    this.$store.commit("loginSuccess", res);
+                    this.$router.push({path: '/'});
+                }
+            ).catch( (error) =>{
+                this.$store.commit("loginFailed" , {error});
+            } );
+        }
+    }
+}
+</script>
